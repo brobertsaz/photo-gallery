be544e5d01cb2d5afc743a388422e0d89484692d
Bob Roberts <cardsfanbob@gmail.com>
2020-06-21 19:50:13 -0500

    Started adding the lotr peeps

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,26 +1,83 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonItem, IonLabel, IonGrid, IonRow, IonCol, IonItemSliding, IonItemOptions, IonItemOption } from '@ionic/react'
+import './Tab1.css'
+
+interface Character {
+  name: string;
+  _id: string;
+  height: string;
+  race: string;
+  gender: string;
+  birth: string;
+  spouse: string;
+  death: string;
+  realm: string;
+  hair: string;
+  wikiUrl: URL;
+}
+
+interface Characters {
+  name: string;
+  matches: Array<Character>;
+}
+
 
 const Tab1: React.FC = () => {
+  const [data, setData] = useState<Characters[]>([])
+  const [showLoading, setShowLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const url = 'https://the-one-api.herokuapp.com/v1/character'
+      const result = await axios.get(url, {
+        params: {},
+        headers: {
+          Authorization: 'Bearer K5RVkjJBo9h9zlhvCrUt'
+        },
+      })
+      setData(result.data.docs)
+      setShowLoading(false)
+    }
+
+    fetchData()
+  }, [])
+
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>LOTR Characters</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <IonHeader collapse="condense">
+        <IonHeader collapse='condense'>
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size='large'>LOTR Characters</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+
+        {data.map((m: any) => (
+          <IonList key={m._id} lines='none'>
+            <IonItemSliding>
+              <IonItem>
+                <IonLabel>
+                  <h2>{m.name}</h2>
+                  <p>{m.race}</p>
+                  <p>{m.wikiUrl}</p>
+                </IonLabel>
+              </IonItem>
+              <IonItemOptions side='end'>
+                <IonItemOption onClick={() => {}}>View Details</IonItemOption>
+              </IonItemOptions>
+            </IonItemSliding>
+          </IonList>
+        ))}
       </IonContent>
     </IonPage>
-  );
+  )
 };
 
 export default Tab1;
+
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -13,9 +13,7 @@ import {
   IonRow,
   IonCol,
   IonImg,
-  IonActionSheet,
 } from '@ionic/react'
-import ExploreContainer from '../components/ExploreContainer';
 import { usePhotoGallery } from '../hooks/usePhotoGallery'
 
 import './Tab2.css';
